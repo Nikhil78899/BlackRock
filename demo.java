702551e8a09e9d8ca53672df37b6107ed4972179
NickSick <nikhilmaldhure195@gmail.com>
2025-07-13 19:29:16 +0530

    added java file

---
+++ demo.java
@@ -0,0 +1,12 @@
+public class demo{
+
+    public void m1(){
+        System.out.println("gunjan is girl ");
+    }
+    
+    public static void main(String [] args){
+        demo demo1 = new demo();
+        demo1.m1();
+
+    }
+}
